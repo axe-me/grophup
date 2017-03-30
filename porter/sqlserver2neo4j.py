f599b20d539283d7b9e339825cb596a9afb6fc57
ShaneXie <shane369@gmail.com>
2017-03-30 03:03:34 -0500

    sql server connection works now

--- porter/sqlserver2neo4j.py
+++ porter/sqlserver2neo4j.py
@@ -1,13 +1,18 @@
 import pymssql
 
-conn = pymssql.connect(server = '(local)', database = "GroupData1_Data")
+conn = pymssql.connect(
+    server='SX-DEV',
+    database="GroupData1_Data"
+)
+
 cursor = conn.cursor()
 
 cursor.execute('SELECT TOP 1000 * FROM Group10')
+
 row = cursor.fetchone()
+
 while row:
     print(row)
-    print("ID=%d, Name=%s" % (row[0], row[1]))
     row = cursor.fetchone()
 
 conn.close()
